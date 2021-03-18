bbffa86f69076afe7f995c3b78cfdef0094e082c
y0n1g33 <80804710+y0n1g33@users.noreply.github.com>
2021-03-17 23:10:43 -0400

    added boom to app.js
    trying this out to see if it works.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Boom!
+        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
